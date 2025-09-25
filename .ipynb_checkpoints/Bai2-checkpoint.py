a16c7ad340b18ed900c185188aa6fac74d6183bb
ngocphuoc317 <ptngocphuoc310725@gmail.com>
2025-09-25 08:29:11 +0000

    Thêm Bài tập thực hành 1

---
+++ .ipynb_checkpoints/Bai2-checkpoint.py
@@ -0,0 +1,10 @@
+# Nhập bán kính
+R = float(input("Nhập bán kính R: "))
+
+# Tính diện tích và chu vi
+S = 3.14 * R * R
+P = 2 * 3.14 * R
+
+# In kết quả với 1 số lẻ thập phân
+print(f"Diện tích hình tròn: {S:.1f}")
+print(f"Chu vi hình tròn: {P:.1f}")
