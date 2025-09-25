c8c8b6ab9d8272bb24d3e821e426c16d7f5d6475
ngocphuoc317 <ptngocphuoc310725@gmail.com>
2025-09-25 15:53:36 +0700

    Delete Bai1.py

--- Bai1.py
+++
@@ -1,5 +0,0 @@
-print("**************************")
-print("* Truong: Dai Hoc UEH     *")
-print("* Khoa: CNTT              *")
-print("* Ho ten:                 *")
-print("**************************")
